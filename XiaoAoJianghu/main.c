485a6ee1e818036799c6ef9204ded5b79543db12
Jimmy-github <m18511699527@gmail.com>
2017-06-27 15:26:22 +0800

    tets1

---
+++ XiaoAoJianghu/main.c
@@ -0,0 +1,37 @@
+//
+//  main.c
+//  XiaoAoJianghu
+//
+//  Created by 张添杰 on 2017/6/25.
+//  Copyright © 2017年 张添杰. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Game.h"
+#include "GameLib.h"
+
+
+
+
+
+
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    SetTitle("笑傲江湖之-精忠报国");
+    ShowWelcome("欢迎来到笑傲江湖世界!!!\n");
+    ShowMap();
+    ShowInformation();
+    ShowMianMenu();
+    
+    printf("test");
+    
+    
+    return 0;
+}
+
+
